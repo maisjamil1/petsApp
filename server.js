55d106e55923031a8e38234201df1471d4a14c24
Tommalieh <muhammedtommalieh@gmail.com>
2020-04-20 09:37:07 +0300

    Search feature done

--- server.js
+++ server.js
@@ -1,3 +1,302 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 'use strict'
 
 require('dotenv').config();
@@ -18,25 +317,110 @@ const app = express();
 
 // const client = new pg.Client(process.env.DATABASE_URL);
 
-app.get('/', (req, res) => {
+app.use(express.static('./public'));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(methodOverride('_method'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', homePage);
+app.get('/searches/new', searchPage);
+app.post('/searches/', searchResults);
+
+
+/////////////// Tommalieh ///////////////////////////////////////////////////
+
+function homePage(req, res){
+
     const url = 'https://api.petfinder.com/v2/animals'
-    superagent.get(url).set({ 'Authorization':`Bearer ${process.env.PETFINDER_API_KEY}` })
-    .then(apiData => {
-        // const pet = apiData.body.animals;
-        // let pets = pet.map(pet => {
-        //     const createdPet = new Pet(pet);
-        //     return createdPet;
-        // })
-        // res.send(pets);
-        res.json(apiData.body);
-    })
-    .catch((err, req, res) => console.log(err))
-})
+    superagent.get(url).set({ 'Authorization': `Bearer ${process.env.PETFINDER_API_KEY}` })
+        .then(apiData => {
+            const pet = apiData.body.animals;
+            let pets = [];
+            pet.forEach(petData => {
+                // console.log(petData.photos.length)
+                if (petData.photos.length !== 0) {
+                console.log('>0')
+                const createdPet = new Pet(petData);
+                pets.push(createdPet);
+                }
+                else {
+                console.log('0')
+                    '';
+                }
+            })
+            // res.send(pets);
+            // res.json(apiData.body); 
+            console.log(pets[0])
+            res.render('./pages/index', { pets: pets })
+        })
+        .catch((err, req, res) => console.log(err))
+};
+
+function searchPage(req, res){
+    res.render('./pages/searches/new');
+};
+
+function searchResults(req, res){
+    const location = req.body.location;
+    const type = req.body.type;
+    const gender = req.body.gender;
+    const url = `https://api.petfinder.com/v2/animals?location=${location}&type=${type}&gender=${gender ? gender : ''}`
+    superagent.get(url).set({ 'Authorization': `Bearer ${process.env.PETFINDER_API_KEY}` })
+        .then(apiData => {
+            console.log(apiData.body)
+            const pet = apiData.body.animals;
+            let pets = [];
+            pet.forEach(petData => {
+                // console.log(petData.photos.length)
+                if (petData.photos.length !== 0) {
+                console.log('>0')
+                const createdPet = new Pet(petData);
+                pets.push(createdPet);
+                }
+                else {
+                console.log('0')
+                    '';
+                }
+            })
+            // res.send(pets);
+            // res.json(apiData.body); 
+            // console.log(pets[0])
+            res.render('./pages/searches/show', { pets: pets })
+        })
+        .catch((err, req, res) => console.log(err))
+};
 
 
-function Pet (petApiData){
-    this.name = petApiData.name;
+
+
+
+
+function Pet(petApiData) {
+    this.pet_id = petApiData.id;
+    this.pet_type = petApiData.type;
+    this.pet_name = petApiData.name;
+    this.pet_species = petApiData.species;
+    this.pet_breed = petApiData.breeds.primary;
+    this.pet_colors = [petApiData.colors.primary, petApiData.colors.secondary, petApiData.colors.tertiary];
+    this.pet_age = petApiData.age;
+    this.pet_gender = petApiData.gender;
+    this.pet_size = petApiData.size;
+    this.pet_is_trained = petApiData.attributes.house_trained;
+    this.pet_is_vacsinated = petApiData.attributes.shots_current;
+    this.pet_description = petApiData.description;
+    this.pet_image_url = petApiData.photos[0];
+    this.contact_email = petApiData.contact.email;
+    this.contact_mobile = petApiData.contact.phone;
+    this.contact_city = petApiData.contact.address.city;
+    this.contact_state = petApiData.contact.address.state;
+    console.log(this);
 }
 
 
 app.listen(PORT, () => console.log(`We're live on port ${PORT} BB ^ o ^`));
+
+/////////////// Tommalieh ///////////////////////////////////////////////////     
