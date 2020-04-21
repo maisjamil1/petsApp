9a94341863d511bdfea66008d14aff07efe8cac8
Tommalieh <muhammedtommalieh@gmail.com>
2020-04-21 10:01:51 +0300

    Add and delete features

--- server.js
+++ server.js
@@ -39,11 +39,14 @@ app.set('view engine', 'ejs');
 app.get('/', homePage);
 app.get('/searches/new', searchPage);
 app.post('/searches/', searchResults);
+app.get('/pets/:petID', showPetDetails);
+app.post('/pets/', addPetToAdopted);
+app.get('/adoptedpets/', showAdoptedPets);
 
 
 /////////////// Tommalieh ///////////////////////////////////////////////////
 
-function homePage(req, res){
+function homePage(req, res) {
 
     const url = 'https://api.petfinder.com/v2/animals'
     superagent.get(url).set({ 'Authorization': `Bearer ${process.env.PETFINDER_API_KEY}` })
@@ -53,12 +56,12 @@ function homePage(req, res){
             pet.forEach(petData => {
                 // console.log(petData.photos.length)
                 if (petData.photos.length !== 0) {
-                console.log('>0')
-                const createdPet = new Pet(petData);
-                pets.push(createdPet);
+                    console.log('>0')
+                    const createdPet = new Pet(petData);
+                    pets.push(createdPet);
                 }
                 else {
-                console.log('0')
+                    console.log('0')
                     '';
                 }
             })
@@ -70,12 +73,12 @@ function homePage(req, res){
         .catch((err, req, res) => console.log(err))
 };
 
-function searchPage(req, res){
+function searchPage(req, res) {
     res.render('./pages/searches/new');
 };
 
 
-function searchResults(req, res){
+function searchResults(req, res) {
     const location = req.body.location;
     const type = req.body.type;
     const gender = req.body.gender;
@@ -88,12 +91,12 @@ function searchResults(req, res){
             pet.forEach(petData => {
                 // console.log(petData.photos.length)
                 if (petData.photos.length !== 0) {
-                console.log('>0')
-                const createdPet = new Pet(petData);
-                pets.push(createdPet);
+                    console.log('>0')
+                    const createdPet = new Pet(petData);
+                    pets.push(createdPet);
                 }
                 else {
-                console.log('0')
+                    console.log('0')
                     '';
                 }
             })
@@ -107,63 +110,109 @@ function searchResults(req, res){
 
 //////////////////////////////// Ahmad ///////////////////////////////////////////////
 
-app.get('/pets/:petsID' ,(req,res)=>{
-    const petsID = [req.params.petsID];
-    console.log('petsID',petsID);
-    const SQL = 'SELECT * FROM pets WHERE id=$1';
-    // client.query(SQL , petsID).then((petsDetails)=>{
-        // console.log(petsDetails);
-        // if(petsDetails.rows.length !== 0){
-        //     console.log('fromDataBase');
-        //     res.render('./pages/pets/show', {pet : petsDetails.rows[0]})
 
-        // } else {
-            const url = 'https://api.petfinder.com/v2/animals'
-            superagent.get(url).set({ 'Authorization':`Bearer ${process.env.PETFINDER_API_KEY}` })
-            .then((petsDetails)=>{
-                // console.log(petsDetails.body);
-                console.log('website')
-                // let pets =[];
-                // const newPets = new Pet(petsDetails.body.animals);
-                // pets.push(newPets);
+function showPetDetails(req, res) {
+    const petID = [req.params.petID];
+    console.log('petID', petID);
+    const SQL = 'SELECT * FROM pets WHERE pet_id = $1';
+    client.query(SQL, petID).then((petsDetails) => {
+        console.log(petsDetails);
+        if (petsDetails.rows.length !== 0) {
+            console.log('fromDataBase');
+            res.render('./pages/pets/show', { pet: petsDetails.rows[0] })
+        }
 
-                res.render('./pages/pets/show' , { pet : petsDetails.body.animals })
-            // })
-        // }
+        else {
+            const url = `https://api.petfinder.com/v2/animals/${petID}`;
+            superagent.get(url).set({ 'Authorization': `Bearer ${process.env.PETFINDER_API_KEY}` })
+                .then(apiData => {
+                    const petData = apiData.body.animal;
+                    const createdPet = new Pet(petData);
+                    res.render('./pages//pets/show', { pet: createdPet })
+                })
+                .catch((err, req, res) => console.log(err))
+        }
+    })
+        .catch((err, req, res) => console.log(err))
+}
 
-    }).catch((err)=> console.log(err));
-})
 
 
-app.get('/pets/:petID', showFun);
+function addPetToAdopted(req, res) {
+    const petID = req.body.petID;
+    console.log(petID);
+    const SQL = 'SELECT * FROM pets WHERE pet_id = $1'
+    client.query(SQL, [petID]).then(result => {
+        if (result.rows.length !== 0) {
+            console.log('stored 2')
+            res.redirect(`/adoptedpets/`)
+        }
+        else {
+            console.log('not stored 2')
+            const url = `https://api.petfinder.com/v2/animals/${petID}`;
+            superagent.get(url).set({ 'Authorization': `Bearer ${process.env.PETFINDER_API_KEY}` })
+                .then(apiData => {
+                    // res.status(200).json(apiData.body)
+                    const petData = apiData.body.animal;
+                    const pet = new Pet(petData);
+                    const SQL = 'INSERT INTO pets (pet_id, pet_type, pet_name, pet_species, pet_breed, pet_colors, pet_age, pet_gender, pet_size, pet_is_trained, pet_is_vacsinated, pet_description, pet_image_url, contact_email, contact_mobile, contact_city, contact_state) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17) RETURNING *'
+                    const values = [
+                    pet.pet_id, pet.pet_type, pet.pet_name, pet.pet_species, pet.pet_breed, 
+                    pet.pet_colors, pet.pet_age, pet.pet_gender, pet.pet_size, pet.pet_is_trained, 
+                    pet.pet_is_vacsinated, pet.pet_description, pet.pet_image_url, pet.contact_email, 
+                    pet.contact_mobile, pet.contact_city, pet.contact_state
+                ]
+                    client.query(SQL, values).then(result => {
+                        res.redirect(`/adoptedpets/`)
+                        // res.status(200).json(result.rows[0]);
+                    })
+                }).catch((err) => {
+                    console.log(err);
+                    // errorHandler(err, req, res);
+                });
+        }
+    })
+}
 
-function showFun(req, res) {
-    let petID = req.params.petID;
-    // console.log(petID);
 
-    let sql = `SELECT * FROM pets WHERE petID = $1;`
-    client.query(sql,[petID])
-      .then(result=>{
-    if (result.rows !== 0 ){
-        res.render('./adopted/show', { data: result.rows[0] });
-    }else {  
-        const url = `https://api.petfinder.com/v2/animals/${petID}`;
-        superagent.get(url)
-        .then(result => {
-            res.render('./adopted/show', { data: result.body });
-            console.log(result.body);
-            
-        });
-  }
-})
-}  
+function showAdoptedPets(req, res) {
+    const SQL = 'SELECT * FROM pets '
+    client.query(SQL).then(result => {
+        console.log('RESULT DATA', result.rows);
+        res.render('./pages/adopted/show', { pets: result.rows })
+    })
+}
+
+
+// app.get('/pets/:petID', showFun);
+
+// function showFun(req, res) {
+//     let petID = req.params.petID;
+//     // console.log(petID);
+
+//     let sql = `SELECT * FROM pets WHERE petID = $1;`
+//     client.query(sql,[petID])
+//       .then(result=>{
+//     if (result.rows !== 0 ){
+//         res.render('./adopted/show', { data: result.rows[0] });
+//     }else {  
+//         const url = `https://api.petfinder.com/v2/animals/${petID}`;
+//         superagent.get(url)
+//         .then(result => {
+//             res.render('./adopted/show', { data: result.body });
+//             console.log(result.body);
+
+//         });
+//   }
+// })
+// }  
 
 
 ////////////////////////////////// Ahmad /////////////////////////////////////////
 
-// curl -d "grant_type=client_credentials&client_id=fEhubHznuC430W5elpJg9HgdPjRJYCpkB0iC3oM7EkxYzwsWmH&client_secret=kYipSPxEc9hKsjuUpaMUwClWGk2om5rs6aCcizLX" https://api.petfinder.com/v2/oauth2/token
+// curl -d "grant_type=client_credentials&client_id=ETHzj63pOADq1dtarMeN88FtVGQZsVkiqAH46NYLTdNLRjrDF8&client_secret=b0i0M466DoJHGvZRCok92uRmOxNwvXkOVa9wUJIj" https://api.petfinder.com/v2/oauth2/token
 
-////////////////////////////////////////////////// ahmad  ///////////////////////////////////////////
+///////////////////////////////// Ahmad  /////////////////////////////////////////
 
 function Pet(petApiData) {
     this.pet_id = petApiData.id;
@@ -186,13 +235,14 @@ function Pet(petApiData) {
     console.log(this);
 }
 
-app.listen(PORT, () => console.log(`We're live on port ${PORT} BB ^ o ^`));
+// app.listen(PORT, () => console.log(`We're live on port ${PORT} BB ^ o ^`));
 
-  
-// client.connect()
-//     .then(() => {
-      
-//         app.listen(PORT, () => console.log(`We're live on port ${PORT} BB ^ o ^`));
-//       })
+
+client.connect()
+    .then(() => {
+
+        app.listen(PORT, () => console.log(`We're live on port ${PORT} BB ^ o ^`));
+    })
+    .catch(err => console.log(err))
 
 /////////////// Tommalieh ///////////////////////////////////////////////////     
